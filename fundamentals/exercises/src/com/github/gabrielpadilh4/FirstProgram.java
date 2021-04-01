b7fefa1e409fc55c393d738850f746c643ee3904
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-03-31 22:13:05 -0300

    First Program

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/FirstProgram.java
@@ -0,0 +1,15 @@
+package com.github.gabrielpadilh4;
+
+public class FirstProgram {
+
+    public static void main(String[] args){
+
+        String version;
+
+        version = System.getProperty("java.version");
+
+        System.out.println("First Programm using Java: " + version);
+    }
+    
+
+}
