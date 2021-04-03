c29b3cf2e02bdaea00518a155d858ac1e6fcfe3c
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-03 16:24:44 -0300

    Primitive Types;

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/PrimitiveTypes.java
@@ -0,0 +1,34 @@
+package com.github.gabrielpadilh4;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        byte yearsOfService = 23;
+        short numberOfFlights = 542;
+        int id = 56789;
+        long points = 3123484223L; //literal
+
+        float salary = 1.44F;
+        double sells = 2991797103.01;
+
+        boolean isOnVacation = false;
+
+        char status = 'A';
+
+
+        // Days of Service
+        System.out.println(yearsOfService * 365);
+
+        // Number of Flights
+        System.out.println(numberOfFlights / 2);
+
+        // Points
+        System.out.println(points / sells);
+
+        System.out.println(id + ": compensations -> " + salary);
+
+        System.out.println("Vaction ?" + (isOnVacation ? "yes" : "no"));
+
+    }
+}
