143172d183e39755f2b340690e98a80a5aab3b57
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-03 22:02:29 -0300

    Conversion between String and Number

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/StringToNumberConversion.java
@@ -0,0 +1,23 @@
+package com.github.gabrielpadilh4;
+
+import javax.swing.*;
+
+public class StringToNumberConversion {
+
+    public static void main(String[] args) {
+        String value1 = JOptionPane.showInputDialog("Enter the first value:");
+        String value2 = JOptionPane.showInputDialog("Enter the second value:");
+
+        System.out.println(value1 + value2);
+
+        double number1 = Double.parseDouble(value1);
+        double number2 = Double.parseDouble(value2);
+
+        double sum = number1 + number2;
+
+        double avg = sum /2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Avg: " + avg);
+    }
+}
