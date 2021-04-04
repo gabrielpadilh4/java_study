2ae1ab9da875e4a6abfb1d509bce37c9c0b6fb9e
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-03 21:31:19 -0300

    Primitive vs Object

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/PrimitiveVsObject.java
@@ -0,0 +1,14 @@
+package com.github.gabrielpadilh4;
+
+public class PrimitiveVsObject {
+
+    public static void main(String[] args) {
+
+        String s = new String("text");
+
+        s.toUpperCase();
+
+        // Wrappers is a object version of primitive types
+        int a = 10;
+    }
+}
