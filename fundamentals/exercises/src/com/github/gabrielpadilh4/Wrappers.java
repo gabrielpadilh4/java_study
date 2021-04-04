17f91e0d5b9d4fe7d00b932b31364b30524bfd97
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-03 21:42:05 -0300

    Wrappers

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/Wrappers.java
@@ -0,0 +1,18 @@
+package com.github.gabrielpadilh4;
+
+public class Wrappers {
+
+    public static void main(String[] args) {
+
+        Byte b = 1;
+        Short s = 100;
+        Integer i = 10000; // int
+        Long l = 100000L;
+        Float f = 1000.123F;
+
+        Boolean bo = true;
+
+        Character c = '#';
+
+    }
+}
