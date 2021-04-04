e3ec7c7720391302b882b7df3b02140c450d0e77
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 18:28:37 -0300

    Unary Operators

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/UnaryOperators.java
@@ -0,0 +1,18 @@
+package com.github.gabrielpadilh4;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        // Postfix
+        a++; // a = a + 1;
+        a--; // a = a - 1;
+
+        // Prefix
+        ++b; // b = b + 1;
+        --b; // b = b - 1;
+    }
+
+}
