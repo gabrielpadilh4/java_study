74e72d90a809a94f153a7cd4a61a0c4fc5092e3b
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 18:18:15 -0300

    Relational Operators

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/RelationalOperators.java
@@ -0,0 +1,17 @@
+package com.github.gabrielpadilh4;
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 'a'; // unicode
+
+        System.out.println(a == b);
+
+        System.out.println(3 >= 4);
+
+        System.out.println(3 < 7);
+
+        System.out.println(30 <= 7);
+    }
+}
