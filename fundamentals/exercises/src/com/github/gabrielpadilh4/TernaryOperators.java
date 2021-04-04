5ccb5ffa42aefd4d0ffb0c9c1dfea87f04b360dc
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 18:40:03 -0300

    Ternary Operators

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/TernaryOperators.java
@@ -0,0 +1,11 @@
+package com.github.gabrielpadilh4;
+
+public class TernaryOperators {
+
+    public static void main(String[] args) {
+        double average = 7.6;
+        String result = average > 7.0 ? "Pass" : "Fail";
+
+        System.out.printf("The student %s in the test", result);
+    }
+}
