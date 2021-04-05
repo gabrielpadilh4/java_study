6aabd72642caa3f63320d06d2d77f157f9e57eaf
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 22:10:43 -0300

    Exercise: Find delta value of bhaskara formula

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/BhaskaraCalculator.java
@@ -0,0 +1,17 @@
+package com.github.gabrielpadilh4;
+
+public class BhaskaraCalculator {
+
+    public static void main(String[] args) {
+
+        double a = 1;
+        double b = 12;
+        double c = -13;
+
+        double delta = Math.pow(b,2) - 4 * a * c;
+
+        System.out.println(delta);
+
+
+    }
+}
