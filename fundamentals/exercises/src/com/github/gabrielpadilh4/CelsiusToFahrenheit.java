d7fd1ac0e6626826a42f8f4391b6c01b6978f97c
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 21:51:26 -0300

    Exercise: Convert Celsius to Fahrenheit

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/CelsiusToFahrenheit.java
@@ -0,0 +1,19 @@
+package com.github.gabrielpadilh4;
+
+public class CelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+
+        final double ADJUSTMENT = 32;
+        final double FACTOR = 9 / 5.0;
+
+        double fahrenheit;
+        double celsius;
+
+        celsius = 30;
+
+        fahrenheit = (celsius * FACTOR) + ADJUSTMENT;
+
+        System.out.printf("The result is %2f °F", fahrenheit);
+    }
+}
