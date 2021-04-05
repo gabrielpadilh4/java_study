2c38ec4680af9719156a676bf491a22bf9d7184d
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-04 21:58:17 -0300

    Exercise: Calculate IMC

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/CalculateIMC.java
@@ -0,0 +1,23 @@
+package com.github.gabrielpadilh4;
+
+import java.util.Scanner;
+
+public class CalculateIMC {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Weight: ");
+        double weight = sc.nextInt();
+
+        System.out.println("Height: ");
+        double height = sc.nextDouble();
+
+        double imc = weight / Math.pow(height,2);
+
+        System.out.printf("Your IMC is: %.2f", imc);
+
+        sc.close();
+    }
+}
