055c5c3c74800dd1387f2f5dd6b61b654b6cc1c6
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-06 22:27:54 -0300

    Control Structures: For

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/control_structures/For.java
@@ -0,0 +1,11 @@
+package com.github.gabrielpadilh4.control_structures;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int counter = 0; counter < 10; counter ++){
+            System.out.println(counter);
+        }
+
+    }
+}
