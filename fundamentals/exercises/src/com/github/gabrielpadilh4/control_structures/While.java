bf53879112daf6cd697845f9d5b21fbe23b2817c
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-06 22:23:50 -0300

    Control Structures: While

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/control_structures/While.java
@@ -0,0 +1,16 @@
+package com.github.gabrielpadilh4.control_structures;
+
+public class While {
+
+    public static void main(String[] args) {
+        int counter = 1;
+
+        while(counter <= 10){
+
+            System.out.println("Count: " + counter);
+
+            counter++;
+        }
+
+    }
+}
