ccc985e7faf0e8b68820c4d6afa8bc5a9dfbc526
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-07 23:36:09 -0300

    Control Structure: Switch without using break;

--- fundamentals/exercises/src/com/github/gabrielpadilh4/control_structures/SwitchWithoutBreak.java
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/control_structures/SwitchWithoutBreak.java
@@ -1,2 +1,31 @@
-package com.github.gabrielpadilh4.control_structures;public class SwitchWithoutBreak {
+package com.github.gabrielpadilh4.control_structures;
+
+import java.util.Locale;
+
+public class SwitchWithoutBreak {
+
+    public static void main(String[] args) {
+
+        String belt = "purple";
+
+        switch (belt.toLowerCase()) {
+
+            case "black":
+                System.out.println("Bassai-dai");
+            case "brown":
+                System.out.println("Tekki Shodan");
+            case "purple":
+                System.out.println("Heian Godan");
+            case "green":
+                System.out.println("Heian Yodan");
+            case "orange":
+                System.out.println("Heian Sandan");
+            case "red":
+                System.out.println("Heian Nidan");
+            case "yellow":
+                System.out.println("Heian Shodan");
+            // default:
+            // System.out.println("I don't know :(");
+        }
+    }
 }
