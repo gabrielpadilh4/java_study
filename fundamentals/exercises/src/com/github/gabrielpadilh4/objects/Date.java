f40d0728d9aa4a6abb45bce3442dd03724a2ea42
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-09 22:55:54 -0300

    Exercise: Create a class named Date with attributes Day, Month and Year. After this, instance the class and print attributes

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/objects/Date.java
@@ -0,0 +1,13 @@
+package com.github.gabrielpadilh4.objects;
+
+public class Date {
+
+    int day;
+    int month;
+    int year;
+
+    public String toString(){
+        return day+"/"+month+"/"+year;
+    }
+
+}
---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/objects/DateTest.java
@@ -0,0 +1,14 @@
+package com.github.gabrielpadilh4.objects;
+
+public class DateTest {
+
+    public static void main(String[] args) {
+        Date date1 = new Date();
+
+        date1.day = 21;
+        date1.month = 9;
+        date1.year = 1996;
+
+        System.out.println(date1.toString());
+    }
+}
