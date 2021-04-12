ba0f111afdbc345645f4e0416c9b5a1944fc11b9
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-11 22:02:43 -0300

    Objects: Use of this and this()

--- fundamentals/exercises/src/com/github/gabrielpadilh4/objects/Date.java
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/objects/Date.java
@@ -7,20 +7,17 @@ public class Date {
     int year;
 
     Date() {
-        day = 1;
-        month = 1;
-        year = 1970;
+        this(1, 1, 170);
     }
 
-    Date(int initialDay, int initialMonth, int initialYear) {
-
-        day = initialDay;
-        month = initialMonth;
-        year = initialYear;
+    Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     public String toString() {
-        return String.format("%d/%d/%d", day, month, year);
+        return String.format("%d/%d/%d", this.day, this.month, this.year);
     }
 
 }
