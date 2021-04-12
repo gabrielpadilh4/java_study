4a37eccaa91b555c9e50951b156cebcdf95945ba
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-04-11 21:55:35 -0300

    Exercise: First Trauma - Access the value of a intance variable in a static method

---
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/objects/FirstTrauma.java
@@ -0,0 +1,15 @@
+package com.github.gabrielpadilh4.objects;
+
+// Exercise access the value of a
+public class FirstTrauma {
+
+    int a; // you cant change this line
+
+    public static void main(String[] args) {
+        // You have to access from here
+
+        FirstTrauma t = new FirstTrauma();
+
+        System.out.println(t.a);
+    }
+}
