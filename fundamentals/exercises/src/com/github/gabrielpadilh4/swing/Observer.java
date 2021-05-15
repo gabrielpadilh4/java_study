275f55b62df47f950f997edd2d556a770a75dc9d
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-05-14 23:23:57 -0300

    Simple swing example

--- fundamentals/exercises/src/com/github/gabrielpadilh4/swing/Observer.java
+++ fundamentals/exercises/src/com/github/gabrielpadilh4/swing/Observer.java
@@ -1,4 +1,27 @@
 package com.github.gabrielpadilh4.swing;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Observer {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Observer");
+
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(600, 200);
+        window.setLayout(new FlowLayout());
+        window.setLocationRelativeTo(null);
+
+        JButton button = new JButton("Click");
+
+        window.add(button);
+
+        button.addActionListener(e -> {
+            System.out.println("Clicked");
+        });
+
+        window.setVisible(true);
+    }
 }
